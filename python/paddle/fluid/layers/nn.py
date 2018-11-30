7761bd5df9739516905c97d539537f149edddcf5
Tink_Y <31891223+tink2123@users.noreply.github.com>
2018-11-30 10:51:28 +0800

    Update nn.py

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -749,14 +749,14 @@ def dynamic_gru(input,
 
     Examples:
 	    .. code-block:: python
-			
-					dict_dim, emb_dim = 128, 64
-					data = fluid.layers.data(name='sequence', shape=[1],
-																	 dtype='int32', lod_level=1)
-					emb = fluid.layers.embedding(input=data, size=[dict_dim, emb_dim])
-					hidden_dim = 512
-					x = fluid.layers.fc(input=emb, size=hidden_dim * 3)
-					hidden = fluid.layers.dynamic_gru(input=x, size=hidden_dim)
+	    
+		dict_dim, emb_dim = 128, 64
+		data = fluid.layers.data(name='sequence', shape=[1],
+					 dtype='int32', lod_level=1)
+		emb = fluid.layers.embedding(input=data, size=[dict_dim, emb_dim])
+		hidden_dim = 512
+		x = fluid.layers.fc(input=emb, size=hidden_dim * 3)
+		hidden = fluid.layers.dynamic_gru(input=x, size=hidden_dim)
     """
 
     helper = LayerHelper('gru', **locals())
