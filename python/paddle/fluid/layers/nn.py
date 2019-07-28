55895f77f2bb878181224b35e0e8528e04173676
tink2123 <y_tink@163.com>
2019-07-28 16:52:26 +0000

    add gather_point op

--- python/paddle/fluid/layers/nn.py
+++ python/paddle/fluid/layers/nn.py
@@ -120,6 +120,7 @@ __all__ = [
     'resize_bilinear',
     'resize_nearest',
     'gather',
+    'gather_point',
     'scatter',
     'sequence_scatter',
     'random_crop',
@@ -8073,6 +8074,48 @@ def gather(input, index, overwrite=True):
                 "Index": index},
         outputs={"Out": out},
         attrs={'overwrite': overwrite})
+    return out
+
+
+def gather_point(input, index):
+    """
+    **Gather Point Layer**
+    Output is obtained by gathering entries of X indexed by `index` 
+    and concatenate them together.
+    .. math::
+        Out = X[Index]
+    .. code-block:: text
+        Given:
+        X = [[1, 2, 3],
+             [3, 4, 5],
+             [5, 6, 7]]
+        Index = [[1, 2]
+        Then:
+        Out = [[3, 4, 5],
+               [5, 6, 7]]
+    Args:
+        input (Variable): The source input with rank>=1, This
+                          is a 3-D tensor with shape of [B, N, 3].
+        index (Variable): The index input with shape = [B, M].
+      
+    Returns:
+        output (Variable): The output is a tensor with shape = [B,M].
+    Examples:
+        .. code-block:: python
+            import paddle.fluid as fluid
+            x = fluid.layers.data(name='x', shape=[-1, 5, 3], dtype='float32')
+            index = fluid.layers.data(name='index', shape=[-1, 1], dtype='int32')
+            output = fluid.layers.gather_point(x, index)
+    """
+
+    helper = LayerHelper('gather_point', **locals())
+    dtype = helper.input_dtype()
+    out = helper.create_variable_for_type_inference(dtype)
+    helper.append_op(
+        type="gather_point",
+        inputs={"X": input,
+                "Index": index},
+        outputs={"Output": out})
     return out
 
 
